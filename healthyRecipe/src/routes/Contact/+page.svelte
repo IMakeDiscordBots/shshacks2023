daeb5bde290e8c0b32a7953b9e25e6afe3b20803
rocketm0509 <rocketm0509@gmail.com>
2023-03-05 04:30:08 -0600

    fixed syntax errors to resolve issues in contacts

--- healthyRecipe/src/routes/Contact/+page.svelte
+++ healthyRecipe/src/routes/Contact/+page.svelte
@@ -1,10 +1,12 @@
 <script>
-    import Header from "../../components/header.svelte"
 
+    import Header from "../../components/header.svelte"
+</script>
 <Header />
 
 <body>
-    <a href=mailto:“isinha24@students.d125.org, bguo24@students.d125.org, mkumar24@students.d125.org, amohanty24@students.d125.org, jefflu24@students.d125.org, jtao25@students.d125.org”>Reach out to us</a>
+    <a href="mailto:“isinha24@students.d125.org, bguo24@students.d125.org, mkumar24@students.d125.org, amohanty24@students.d125.org, jefflu24@students.d125.org, jtao25@students.d125.org”">Reach out to us</a>
+
 </body>
 
 <style>
