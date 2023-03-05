a37284ca48d9e6b69dde57f44dc98dcec8c45b8a
rocketm0509 <rocketm0509@gmail.com>
2023-03-05 05:01:22 -0600

    made the faq page pretty

--- healthyRecipe/src/routes/FAQs/+page.svelte
+++ healthyRecipe/src/routes/FAQs/+page.svelte
@@ -34,4 +34,32 @@
 
 <style>
     
+@import url('https://fonts.googleapis.com/css2?family=Gloock&family=Open+Sans&family=Playfair+Display&family=Raleway&display=swap');
+    :root {
+        --nav-bar: #7D84B2;
+        --nav-text: #D95D39;
+        --primary: #8D98A7;
+        --secondary: #DCCCBB;
+        --tertiary: #646E78;
+    }
+    body {
+        display: flex;
+        justify-content: center;
+        margin: auto;
+        padding: 100px;
+        background-color: #D5F9DE;
+    }
+    .content {
+        margin: auto;
+        border-radius: 10px;
+        background-color: #FAF9F6;
+        padding: 40px;
+        box-shadow: 0 20px 30px -14px rgba(0, 0, 0, 0.25);
+    }
+    h1 {
+        font-family: 'Gloock', serif;
+    }
+    .about {
+        font-family: 'Raleway', sans-serif;
+    }
 </style>
