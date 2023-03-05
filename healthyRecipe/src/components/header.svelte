b1e0f8d0d385032d739c18758c4d9338a78630e2
rocketm0509 <rocketm0509@gmail.com>
2023-03-05 05:04:52 -0600

    changed header components

--- healthyRecipe/src/components/header.svelte
+++ healthyRecipe/src/components/header.svelte
@@ -2,9 +2,9 @@
     <nav class = "navbar">
         <ul class = "nav-items">
             <li class = "nav-item"><a href = "/">Home</a></li>
+            <li class = "nav-item"><a href = "/VideoP">Video</a></li>
             <li class = "nav-item"><a href = "/info">About</a></li>
             <li class = "nav-item"><a href = "/FAQs">FAQs</a></li>
-            <li class = "nav-item"><a href = "/VideoP">Video</a></li>
             <li class = "nav-item"><a href = "/Contact">Contact Us</a></li>
         </ul>
     </nav>
